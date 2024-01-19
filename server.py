80657f335075605b3f91a5b8e00c568523b83634
Adrian Edwards <adrian@adriancedwards.com>
2024-01-19 17:33:08 -0500

    init app

---
+++ server.py
@@ -0,0 +1,8 @@
+from http.server import HTTPSServer
+
+def run(server_class=HTTPSServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
