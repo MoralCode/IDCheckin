342d90f48837a51c7d7257111fbb2e0f4ac876f3
Adrian Edwards <adrian@adriancedwards.com>
2024-01-19 16:25:54 -0500

    fix indentation

--- frontend/main.js
+++ frontend/main.js
@@ -9,25 +9,25 @@ function readNFC() {
 }
 
 function fetchUsername() {
-            const badgeId = document.getElementById('badgeId').value;
+    const badgeId = document.getElementById('badgeId').value;
 
-            // Make a GET request to the server to fetch the username
-            fetch(BASEURL + `/username/${badgeId}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    // Display the username in the result paragraph
-                    document.getElementById('result').textContent = `Username: ${data.username}`;
-                })
-                .catch(error => {
-                    console.error('Error fetching username:', error);
-                    document.getElementById('result').textContent = 'Error fetching username';
-                });
-        }
+    // Make a GET request to the server to fetch the username
+    fetch(BASEURL + `/username/${badgeId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Display the username in the result paragraph
+            document.getElementById('result').textContent = `Username: ${data.username}`;
+        })
+        .catch(error => {
+            console.error('Error fetching username:', error);
+            document.getElementById('result').textContent = 'Error fetching username';
+        });
+}
 
 function submitAttendance() {
     const badgeId = document.getElementById('badgeId').value;
